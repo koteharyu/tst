ce1138d987cf3e57d3558563ade812b649bff3de
haryu <hryau6@gmail.com>
2021-08-27 21:13:29 +0900

    add: validations

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,6 @@
 class Group < ApplicationRecord
   has_many :group_members, dependent: :destroy
   has_many :members, through: :group_members
+
+  validates :name, presence: true
 end
--- app/models/member.rb
+++ app/models/member.rb
@@ -2,4 +2,6 @@ class Member < ApplicationRecord
   has_many :group_members, dependent: :destroy
   has_many :groups, through: :group_members
   has_many :role, through: :group_members
+
+  validates :name, presence: true
 end
