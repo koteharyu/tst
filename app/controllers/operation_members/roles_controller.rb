26aca8939b1aa656b7b1bbe2ecae643d7effcac5
haryu <hryau6@gmail.com>
2021-08-28 13:47:38 +0900

    modify: 幹事の更新をenumのメソッドで変更

--- app/controllers/operation_members/roles_controller.rb
+++ app/controllers/operation_members/roles_controller.rb
@@ -5,11 +5,11 @@ class OperationMembers::RolesController < ApplicationController
     current_organizer = @group.group_members&.find_by(group_id: @group.id, role: 1)
     if current_organizer.nil?
       target_member = @group.group_members&.where(group_id: @group.id, role: 0).sample
-      target_member.update(role: 1)
+      target_member.organizer!
     else
       target_member = @group.group_members&.where(group_id: @group.id, role: 0).sample
-      current_organizer.update(role: 0)
-      target_member.update(role: 1)
+      current_organizer.regular!
+      target_member.organizer!
     end
     redirect_to group_path(@group), notice: '幹事を設定しました'
   end
