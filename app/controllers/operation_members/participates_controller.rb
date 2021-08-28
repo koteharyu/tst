dd74ce08b54e510fc752ec373ad60ec55ad1c2ce
haryu <hryau6@gmail.com>
2021-08-28 13:02:53 +0900

    add: 脱退機能

--- app/controllers/operation_members/participates_controller.rb
+++ app/controllers/operation_members/participates_controller.rb
@@ -3,8 +3,12 @@ class OperationMembers::ParticipatesController < ApplicationController
 
   def update
     new_member_ids = @group.member_ids << participate_params
-    binding.pry
     @group.update(member_ids: new_member_ids)
+    redirect_to group_path(@group)
+  end
+
+  def destroy
+    @group.group_members.find_by(group_id: @group.id, member_id: participate_params.to_i).destroy
     redirect_to group_path(@group)
   end
 
