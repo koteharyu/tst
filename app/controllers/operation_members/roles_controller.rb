32e66f76efedabdb7e9042ea8306240d405512b4
haryu <hryau6@gmail.com>
2021-08-28 13:42:41 +0900

    add: 幹事設定機能のベース

--- app/controllers/operation_members/roles_controller.rb
+++ app/controllers/operation_members/roles_controller.rb
@@ -1,2 +1,22 @@
 class OperationMembers::RolesController < ApplicationController
+  before_action :set_group
+
+  def update
+    current_organizer = @group.group_members&.find_by(group_id: @group.id, role: 1)
+    if current_organizer.nil?
+      target_member = @group.group_members&.where(group_id: @group.id, role: 0).sample
+      target_member.update(role: 1)
+    else
+      target_member = @group.group_members&.where(group_id: @group.id, role: 0).sample
+      current_organizer.update(role: 0)
+      target_member.update(role: 1)
+    end
+    redirect_to group_path(@group), notice: '幹事を設定しました'
+  end
+
+  private
+
+  def set_group
+    @group = Group.find(params[:id])
+  end
 end
