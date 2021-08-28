fc70b03b36f18dcec8b51b24583fe4c21ab4d335
haryu <hryau6@gmail.com>
2021-08-28 12:40:18 +0900

    add: 参加機能

--- app/controllers/operation_members/participates_controller.rb
+++ app/controllers/operation_members/participates_controller.rb
@@ -1,9 +1,20 @@
 class OperationMembers::ParticipatesController < ApplicationController
   before_action :set_group
 
+  def update
+    new_member_ids = @group.member_ids << participate_params
+    binding.pry
+    @group.update(member_ids: new_member_ids)
+    redirect_to group_path(@group)
+  end
+
   private
 
   def set_group
     @group = Group.find(params[:id])
+  end
+
+  def participate_params
+    params[:member_id]
   end
 end
