a0fbb35be7243ee318c17079e17ebbb7cfdbe2b6
haryu <hryau6@gmail.com>
2021-08-29 21:47:49 +0900

    add: participate & withdraw method

--- app/models/group.rb
+++ app/models/group.rb
@@ -7,14 +7,22 @@ class Group < ApplicationRecord
   # scope :set_target_member, -> { joins(:group_members)&.where(group_id: @group.id, role: 0).sample}
 
   def set_current_organizer
-    self.group_members&.find_by(group_id: id, role: 1)
+    group_members&.find_by(group_id: id, role: 1)
   end
 
   def set_target_member
-    self.group_members&.where(group_id: id, role: 0).sample
+    group_members&.where(group_id: id, role: 0).sample
   end
 
   def set_target_member_name(target_member)
-    self.members.find(target_member.member_id).name
+    members.find(target_member.member_id).name
+  end
+
+  def participate(member)
+    members << member
+  end
+
+  def withdraw(member)
+    members.destroy(member)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,5 @@ Rails.application.routes.draw do
     resources :participates, only: [:update, :destroy]
     resources :roles, only: [:update]
   end
-  resources :group_members, only: [:update, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
